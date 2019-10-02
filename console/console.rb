7431766fb8d7e9761f4367820f1ca7c23499c677
Luan Pontes <luanpontes2@gmail.com>
2019-10-01 23:41:27 -0300

    Update console.rb
    Adicionar documentação.

--- console/console.rb
+++ console/console.rb
@@ -1,8 +1,14 @@
 #!/usr/bin/env ruby
 
 require 'irb'
-
 require_relative '../lib/strans-client'
+
+# Entre no projeto e execute '$ bin/console.rb', que abrirar 
+# o irb, então poderá instanciar um client e interagir com a lib. Exemplo: 
+
+# irb> cliente = StransClient.new('oseu@email.com', 'suaSenha', 'suaChaveAqui'
+# irb> linhas = cliente.get(:linhas)
+# irb> puts linhas
 
 IRB.start
 
